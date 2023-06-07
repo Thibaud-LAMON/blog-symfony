7b4a3f2808253022cda7993aca31da996b24e5eb
Thibaud-LAMON <thib.lamon@gmail.com>
2023-06-08 00:07:16 +0200

    articles system

---
+++ src/Model/TimeStampedInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+interface TimeStampedInterface
+{
+    public function getCreatedAt(): ?\DateTimeInterface;
+
+    public function setCreatedAt(\DateTimeInterface $createdAt);
+
+    public function getUpdatedAt(): ?\DateTimeInterface;
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt);
+}
