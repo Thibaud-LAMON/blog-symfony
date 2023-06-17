b4382a40c85c8c5264e5d98308649d67afc7e1a7
Thibaud-LAMON <thib.lamon@gmail.com>
2023-06-17 02:06:46 +0200

    dynamic menu system

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Article;
 use App\Entity\Category;
 use App\Entity\Comment;
+use App\Entity\Menu;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -37,12 +38,21 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToRoute('Aller sur le site', 'fa fa-undo', 'app_home');
+        yield MenuItem::linkToRoute('Aller sur le site', 'fas fa-undo', 'app_home');
         yield MenuItem::subMenu('Articles', 'fas fa-newspaper',)->setSubItems([
             MenuItem::linkToCrud('Tous les articles', 'fas fa-newspaper', Article::class),
             MenuItem::linkToCrud('Ajouter', 'fas fa-plus', Article::class)->setAction(Crud::PAGE_NEW),
             MenuItem::linkToCrud('Catégories', 'fas fa-list', Category::class),
-
+        ]);
+        yield MenuItem::subMenu('Menus', 'fas fa-list')->setSubItems([
+            MenuItem::linkToCrud('Pages', 'fas fa-file', Menu::class)
+                ->setQueryParameter('submenuIndex', 0),
+            MenuItem::linkToCrud('Articles', 'fas fa-newspaper', Menu::class)
+                ->setQueryParameter('submenuIndex', 1),
+            MenuItem::linkToCrud('Liens personnalisés', 'fas fa-link', Menu::class)
+                ->setQueryParameter('submenuIndex', 2),
+            MenuItem::linkToCrud('Catégories', 'fab fa-delicious', Menu::class)
+                ->setQueryParameter('submenuIndex', 3),
         ]);
         yield MenuItem::LinkToCrud('Commentaires', 'fas fa-comment', Comment::class);
     }
